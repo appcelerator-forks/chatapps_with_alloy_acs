13e1a4448bcb1a65dd7828385a6dac1efe7c707c
HiroshiOyamada <h5y1m141@gmail.com>
2015-01-11 08:30:44 +0900

    Facebookアカウントで認証できるように実装

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,45 @@
-$.index.open();
+var fb = require('facebook');
+fb.appid = Ti.App.Properties.getString('ti.facebook.appid');
+fb.permissions = ['publish_stream','email','user_birthday'];
+fb.forceDialogAuth = false;
+fb.addEventListener('login', function(e) {
+  if (e.success) {
+    fb.requestWithGraphPath('me',{},"GET",function(e) {
+        if (e.success) {
+          var obj = JSON.parse(e.result),
+              moment = require('alloy/moment'),
+              token, user, email, birthday;
+
+          email = obj.email;
+          birthday = moment(obj.birthday, "MM/DD/YYYY");
+          token = fb.accessToken;
+          Ti.API.info("Success: " +
+                      "email" + email +
+                      "birthday" + birthday +
+                      "token" + token
+                     );
+          user = Alloy.createModel('social');
+          user.fbLogin(token,email, birthday);
+        }
+      }
+    );
+
+  } else if (e.error) {
+    Ti.API.info(e.error);
+  } else if (e.cancelled) {
+    Ti.API.info("Canceled");
+  }
+});
+
+$.fbloginLabel.addEventListener('click', function(e) {
+  fb.authorize();
+});
+
 
 $.label.addEventListener('click', function(e) {
   var newWindow;
   newWindow = Alloy.createController('newWindow');
-  return newWindow.move($.tabOne);
-});$.index.open();
+  return newWindow.move($.tabTwo);
+});
+
+$.index.open();
